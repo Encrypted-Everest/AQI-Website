c70d1a060a506b4d6adb7e619aa8189da307cbd7
Encrypted-Everest <isameersah888@gmail.com>
2025-02-16 18:40:46 +0545

    Initial commit

---
+++ src/message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing - capitalizes the first letter of each word in the identifier
+
+function Message() {
+    //jsx - javascript xml
+    const name = 'Sameer';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello, go!</h1>;
+    }
+
+export default Message;
